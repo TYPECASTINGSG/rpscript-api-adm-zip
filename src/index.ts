b1c5daab0f2365e4fca93c578bbe6c6cdd7a2e0d
James Chong <wei3hua2@gmail.com>
2018-07-17 17:15:56 +0800

    added adm-zip documentation

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,31 @@ import fs from 'fs';
 import AdmZip, { IZipEntry } from 'adm-zip';
 import {RpsContext,RpsModule,rpsAction} from 'rpscript-interface';
 
-/** Welcome to Basic Module
- * @namespace Basic 
+/** Zipping and extracting files
+ * @see {@link https://www.npmjs.com/package/adm-zip|Zip}
+ * @namespace Zip
+ * 
+ * @example
+ * rps install adm-zip
+ * 
+ * 
 */
 @RpsModule("adm-zip")
 export default class RPSAdmZip {
 
+/**
+ * @function zip
+ * @memberof Zip
+ * @example
+ * ;Write to a file call back.zip, compress temp folder and readme.md file
+ * zip "backup.zip" "./temp/" "./readme.md"
+ * @param {string} zipFile filename of the zip file
+ * @param {Array} entries the list of files or folders to be zipped
+ * @returns {string} the compressed filename 
+ * @summary Zipping file by specifying the filename and the list of entries
+ * 
+ * 
+*/
   @rpsAction({verbName:'zip'})
   async zip (ctx:RpsContext,opts:Object, zipFile:string, ...entries:string[]) : Promise<string>{
     let z = new AdmZip();
@@ -26,7 +45,19 @@ export default class RPSAdmZip {
     return fs.lstatSync(path).isDirectory();
   }
 
-  @rpsAction({verbName:'extract'})
+  /**
+ * @function extract-zip
+ * @memberof Zip
+ * @example
+ * ;Extract the zip file to the folder temp
+ * extract-zip "backup.zip" "./temp/"
+ * @param {string} zipFile filename of the zip file.
+ * @param {string} extractTo directory to extract to.
+ * @returns {string} zip file's name
+ * @summary Extract a zip file.
+ * 
+*/
+  @rpsAction({verbName:'extract-zip'})
   async extractAllTo (ctx:RpsContext,opts:Object, zipFile:string, extractTo:string) : Promise<string>{
     let z = new AdmZip(zipFile);
     
@@ -35,7 +66,20 @@ export default class RPSAdmZip {
     return extractTo;
   }
 
-  @rpsAction({verbName:'getZipEntries'})
+
+  /**
+ * @function get-zip-entries
+ * @memberof Zip
+ * @example
+ * get-zip-entries "backup.zip"
+ * @param {string} zipFile filename of the zip file.
+ * @returns {Array} IZipEntry. The object format which includes the metadata.
+ * @summary Get zip file entries information
+ * 
+ * @see {@link https://www.npmjs.com/package/adm-zip}
+ * 
+*/
+  @rpsAction({verbName:'get-zip-entries'})
   async getZipEntries (ctx:RpsContext,opts:Object, zipFile:string) : Promise<IZipEntry[]>{
     let z = new AdmZip(zipFile);
 
